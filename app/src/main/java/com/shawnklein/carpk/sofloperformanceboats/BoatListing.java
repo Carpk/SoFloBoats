a297cc625c359d1086d5ddd1f2dcfde8e17ddde4
brechen <zappeln@gmail.com>
2016-10-16 15:00:56 -0500

    add landing view
    adjust xml views to center pics
    remove old text used for testing
    add new testing urls that cropped closer to screen aspect ration

--- app/src/main/java/com/shawnklein/carpk/sofloperformanceboats/BoatListing.java
+++ app/src/main/java/com/shawnklein/carpk/sofloperformanceboats/BoatListing.java
@@ -10,10 +10,12 @@ public class BoatListing {
 
     private BoatListing(Context appContext) {
         mBoats = new ArrayList<Boat>();
-        String[] boatArrayUrls = {"http://images.boats.com/resize/1/0/10/5080010_20150505140842676_1_LARGE.jpg",
-                "http://images.boats.com/resize/1/62/41/4096241_-1_20121003075030_4_0.jpg",
-                "http://images.boats.com/resize/1/19/2/5771902_20160411201510914_1_LARGE.jpg"};
-        for (int i = 0; i < 3; i++) {
+        String[] boatArrayUrls = {"http://media.channelblade.com/boat_graphics/dealers/40524/digi55489199_l.jpg",
+                "http://media.channelblade.com/boat_graphics/dealers/40524/digi56183663_l.jpg",
+                "http://media.channelblade.com/boat_graphics/dealers/40524/digi55489191_l.jpg",
+                "http://images.boats.com/resize/1/5/12/4710512_20140521135437105_1_LARGE.jpg"};
+
+        for (int i = 0; i < boatArrayUrls.length; i++) {
             Boat b = new Boat();
             b.setTitle("Boat #" + i);
             b.setUrl(boatArrayUrls[i]);
